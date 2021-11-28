70cc0fa4a7c3ed048762f04153c4c4d54a76dee3
Shahzeb Khan <shahzeb8285@gmail.com>
2021-11-28 18:41:59 +0530

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const FACTORY_ADDRESS = '0xf0bc2E21a76513aa7CC2730C7A1D6deE0790751f'
+
+export const BUNDLE_ID = '1'
+
+export const timeframeOptions = {
+  WEEK: '1 week',
+  MONTH: '1 month',
+  // THREE_MONTHS: '3 months',
+  // YEAR: '1 year',
+  ALL_TIME: 'All time',
+}
+
+// token list urls to fetch tokens from - use for warnings on tokens and pairs
+export const SUPPORTED_LIST_URLS__NO_ENS = [
+  'https://raw.githubusercontent.com/pantherswap/panther-frontend-dex/master/src/constants/token/pantherswap.json',
+]
+
+// hide from overview list
+export const OVERVIEW_TOKEN_BLACKLIST = []
+
+// pair blacklist
+export const PAIR_BLACKLIST = []
