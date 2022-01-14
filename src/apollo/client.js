e498d0bbc6b895d009bde53596d7cc6cd551e3ae
Shahzeb Khan <shahzeb8285@gmail.com>
2022-01-15 02:34:19 +0530

    all fine

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/knightswap-dex/knightgraph',
+    uri: 'https://api.thegraph.com/subgraphs/id/QmPQfcovYgjF2vyGBE4LwXaSYj7Bgfvbny8MBpgLSBVKjB',
   }),
   cache: new InMemoryCache(),
   shouldBatch: true,
